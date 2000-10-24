57d78081e0cf5c3ceaae804b30038531169621d6
hothorn <hothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2000-10-24 14:42:41 +0000

    Initial revision
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mvtnorm@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/mvtnorm/trunk/check/mvtnorm-Ex.R
@@ -0,0 +1,64 @@
+attach(NULL, name = "CheckExEnv")
+assign(".CheckExEnv", pos.to.env(2), pos = length(search()))
+assign("ptime", proc.time(), env = .CheckExEnv)
+postscript("mvtnorm-Examples.ps")
+assign("par.postscript", par(no.readonly = TRUE), env = .CheckExEnv)
+options(contrasts = c(unordered = "contr.treatment", ordered = "contr.poly"))
+library('mvtnorm')
+rm(list = ls(all = TRUE)); .Random.seed <- c(0,rep(7654,3))
+###--- >>> `pmvnorm' <<<----- Multivariate Normal Distribution
+
+	## alias	 help(pmvnorm)
+
+##___ Examples ___:
+
+
+# ok, first of all a little univariate test: 
+
+prob <- pmvnorm(mean = 3, corr = 1, lower = -Inf, upper = 3)
+print(prob$value - pnorm(3,3))
+
+# now something more meaningful
+
+n <- 5
+mean <- rep(0, 5)
+lower <- rep(-1, 5)
+upper <- rep(3, 5)
+corr <- diag(5)
+corr[lower.tri(corr)] <- 0.5
+prob <- pmvnorm(mean, corr, lower, upper)
+print(prob)
+
+
+
+
+rm(list = ls(all = TRUE)); .Random.seed <- c(0,rep(7654,3))
+###--- >>> `pmvt' <<<----- Multivariate t Distribution
+
+	## alias	 help(pmvt)
+
+##___ Examples ___:
+
+
+# ok, first of all a little univariate test: 
+
+prob <- pmvt(lower = -Inf, upper = 3, df = 3, corr = 1, delta = 0)
+print(prob$value - pt(3,3))
+
+# now something more meaningful
+
+n <- 5
+lower <- rep(-1, 5)
+upper <- rep(3, 5)
+df <- 4
+corr <- diag(5)
+corr[lower.tri(corr)] <- 0.5
+delta <- rep(0, 5)
+prob <- pmvt(lower, upper, df, corr , delta)
+print(prob)
+
+
+
+
+cat("Time elapsed: ", proc.time() - get("ptime", env = .CheckExEnv),"\n")
+dev.off(); quit('no')
