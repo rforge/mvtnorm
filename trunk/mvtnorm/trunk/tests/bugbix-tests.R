8a9e4be71c8ce0f72ce101a2abd784a4bdd27871
hothorn <hothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2002-04-09 07:08:59 +0000

    check for sig2corr bug
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mvtnorm@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/mvtnorm/trunk/tests/bugbix-tests.R
@@ -0,0 +1,12 @@
+library(mvtnorm)
+
+# correlation matrices for unequal variances were wrong
+# from Pamela Ohman-Strickland <ohmanpa@UMDNJ.EDU>
+
+a = 4.048
+shi = -9
+slo = -10
+mu = -5
+sig = matrix(c(1,1,1,2),ncol=2) 
+pmvnorm(lower=c(-a,slo),upper=c(a,shi),mean=c(mu,2*mu),sigma=sig)
+
