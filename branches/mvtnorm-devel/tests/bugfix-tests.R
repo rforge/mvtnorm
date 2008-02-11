e8761ed1412b1fe4e99690c920e767499b53504c
hothorn <hothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-02-11 17:50:51 +0000

    cosmetics, mostly
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mvtnorm@176 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/mvtnorm-devel/tests/bugfix-tests.R
+++ branches/mvtnorm-devel/tests/bugfix-tests.R
@@ -144,9 +144,9 @@ corr <- diag(5)
 corr[lower.tri(corr)] <- 0.5
 delta <- rep(0, 5)
 set.seed(290875)
-prob1 <- pmvt(lower=lower, upper=upper, delta=delta, df=df, corr=corr)
+prob1 <- pmvnormM(lower=lower, upper=upper, mean = delta, corr=corr)
 set.seed(290875)
-prob2 <- pmvt(lower=lower, upper=upper, delta=delta, df=df, corr=corr)
+prob2 <- pmvnormM(lower=lower, upper=upper, mean = delta, corr=corr)
 stopifnot(all.equal(prob1, prob2))
 
 # confusion for univariate probabilities when sigma is a matrix
@@ -160,26 +160,6 @@ stopifnot(all.equal(a, pnorm(2, sd=sqrt(.5))))
 a <- pmvnormM(lower=-Inf,upper=2,mean=0,sigma=.5)
 attributes(a) <- NULL
 stopifnot(all.equal(a, pnorm(2, sd=sqrt(.5))))
-
-# log argument added by Jerome Asselin <jerome@hivnet.ubc.ca>
-dmvnorm(x=c(0,0), mean=c(1,1),log=TRUE)
-dmvnorm(x=c(0,0), mean=c(25,25),log=TRUE)
-dmvnorm(x=c(0,0), mean=c(30,30),log=TRUE)
-stopifnot(all.equal(dmvnorm(x=0, mean=30,log=TRUE), 
-                    dnorm(0,30,log=TRUE)))
-
-# large df
-pnorm(2)^2
-pmvt(lower=c(-Inf,-Inf), upper=c(2,2), delta=c(0, 0), df=25, corr=diag(2))
-pmvt(lower=c(-Inf,-Inf), upper=c(2,2), delta=c(0, 0), df=250, corr=diag(2))
-pmvt(lower=c(-Inf,-Inf), upper=c(2,2), delta=c(0, 0), df=1340, corr=diag(2))
-pmvt(lower=c(-Inf,-Inf), upper=c(2,2), delta=c(0, 0), df=2500, corr=diag(2))
-pmvt(lower=c(-100,-100), upper=c(2,2), delta=c(0, 0), df=2500, corr=diag(2))
-
-# df = 0
-pmvt(lower=c(-Inf,-Inf), upper=c(2,2), delta=c(0, 0), df=0, corr=diag(2))
-pmvt(lower=-Inf, upper = 2, delta=0, df=0, corr=1)
-pnorm(2)
 
 
 # cases where the support is the empty set tried to compute something.
