871138d398306b7437cec42182907f1222d5a950
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-04-04 09:49:28 +0000

    mv
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mvtnorm@183 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/randomF77.c
@@ -0,0 +1,12 @@
+/* $Id$
+*
+*  wrapper for calling R's random number generator from
+*  the original FORTRAN code
+*
+*/
+
+#include <R.h>
+
+void F77_SUB(rndstart)(void) { GetRNGstate(); }
+void F77_SUB(rndend)(void) { PutRNGstate(); }
+double F77_SUB(unifrnd)(void) { return unif_rand(); }
