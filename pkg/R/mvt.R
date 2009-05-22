fa915dab841b9dfc54cda9e99fa43699bc812614
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-22 09:46:33 +0000

    typo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mvtnorm@197 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/mvt.R
+++ pkg/R/mvt.R
@@ -149,7 +149,7 @@ pmvt <- function(lower=-Inf, upper=Inf, delta=rep(0, length(lower)),
                        delta=carg$mean, algorithm = algorithm, ...)
         }
     }
-    attr(RET$value, "error") <- ifelse(is.na(RET$error), 1, is.na(RET$error))
+    attr(RET$value, "error") <- ifelse(is.na(RET$error), 1, RET$error)
     attr(RET$value, "msg") <- RET$msg
     return(RET$value)
 }
