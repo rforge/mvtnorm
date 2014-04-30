a47d45d32a5834ad7c6e80fd74eea0973771735b
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-30 09:20:55 +0000

    - improvement in rmvnorm() {unused code; faster sandwich formula} - dmvnorm():  returning NaN + warning is less nice than               behaving the same as dnorm(*, sd=0):  return Inf or 0
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mvtnorm@274 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/mvnorm.R
+++ pkg/R/mvnorm.R
@@ -1,45 +1,39 @@
 # $Id$
 
-rmvnorm<-function (n, mean = rep(0, nrow(sigma)), sigma = diag(length(mean)),
+rmvnorm <- function(n, mean = rep(0, nrow(sigma)), sigma = diag(length(mean)),
                    method=c("eigen", "svd", "chol"), pre0.9_9994 = FALSE)
 {    
     if (!isSymmetric(sigma, tol = sqrt(.Machine$double.eps), 
                      check.attributes = FALSE)) {
         stop("sigma must be a symmetric matrix")
     }
-    if (length(mean) != nrow(sigma)) {
+    if (length(mean) != nrow(sigma))
         stop("mean and sigma have non-conforming size")
-    }
-    sigma1 <- sigma
-    dimnames(sigma1) <- NULL
-    if(!isTRUE(all.equal(sigma1, t(sigma1)))){
-        warning("sigma is numerically not symmetric")
-    }
 
     method <- match.arg(method)
     
-    if(method == "eigen"){
+    R <- if(method == "eigen") {
         ev <- eigen(sigma, symmetric = TRUE)
         if (!all(ev$values >= -sqrt(.Machine$double.eps) * abs(ev$values[1]))){
             warning("sigma is numerically not positive definite")
         }    
-        retval <- ev$vectors %*%  diag(sqrt(ev$values), 
-                      length(ev$values)) %*% t(ev$vectors)
+        ## ev$vectors %*% diag(sqrt(ev$values), length(ev$values)) %*% t(ev$vectors)
+        ## faster for large  nrow(sigma):
+        t(ev$vectors %*% (t(ev$vectors) * sqrt(ev$values)))
     }
     else if(method == "svd"){
-        sigsvd <- svd(sigma)
-        if (!all(sigsvd$d >= -sqrt(.Machine$double.eps) * abs(sigsvd$d[1]))){
+        s. <- svd(sigma)
+        if (!all(s.$d >= -sqrt(.Machine$double.eps) * abs(s.$d[1]))){
             warning("sigma is numerically not positive definite")
         }    
-        retval <- t(sigsvd$v %*% (t(sigsvd$u) * sqrt(sigsvd$d)))
+        t(s.$v %*% (t(s.$u) * sqrt(s.$d)))
     }    
     else if(method == "chol"){
-        retval <- chol(sigma, pivot = TRUE)
-        o <- order(attr(retval, "pivot"))
-        retval <- retval[,o]
+        R <- chol(sigma, pivot = TRUE)
+        R[, order(attr(R, "pivot"))]
     }
     
-    retval <- matrix(rnorm(n * ncol(sigma)), nrow = n, byrow = !pre0.9_9994) %*%  retval
+    retval <- matrix(rnorm(n * ncol(sigma)), nrow = n, byrow = !pre0.9_9994) %*%  R
     retval <- sweep(retval, 2, mean, "+")
     colnames(retval) <- names(mean)
     retval
@@ -50,42 +44,45 @@ dmvnorm <- function (x, mean, sigma, log = FALSE, trustme = FALSE)
     if (is.vector(x)) {
         x <- matrix(x, ncol = length(x))
     }
-    if (missing(mean)) {
-        mean <- rep(0, length = ncol(x))
-    }
-    if (missing(sigma)) {
-        sigma <- diag(ncol(x))
-    }
-    if( !is.null(dim(mean)) ) dim(mean) <- NULL
+    p <- ncol(x)
+    if (missing(mean))
+        mean <- rep.int(0, p)
+    else if(!is.null(dim(mean))) dim(mean) <- NULL
 
+    if (missing(sigma)) {
+        sigma <- diag(p)
+    }
+
+    doSolve <- TRUE
+    ## MM: Code would be much so much nicer without the 'trustme' argument...
     if (!trustme) {
-        if (NCOL(x) != NCOL(sigma)) {
+        if (p != ncol(sigma))
             stop("x and sigma have non-conforming size")
-        }
-        if (!isSymmetric(sigma, tol = sqrt(.Machine$double.eps), 
-                         check.attributes = FALSE)) {
+        if (!isSymmetric(sigma, tol = sqrt(.Machine$double.eps),
+                         check.attributes = FALSE))
             stop("sigma must be a symmetric matrix")
-        }
-        if (length(mean) != NROW(sigma)) {
+        if (length(mean) != p)
             stop("mean and sigma have non-conforming size")
-        }
 
         ### <faster code contributed by Matteo Fasiolo mf364 at bath.ac.uk
-        dec <- try(chol(sigma))
-        if (inherits(dec, "try-error")) {
-            warning("cannot compute chol(sigma)")
-            return(NaN)
+        dec <- tryCatch(chol(sigma), error=function(e)e)
+        if (inherits(dec, "error")) {
+            ## warning("cannot compute chol(sigma)"); return(NaN)
+            ## behave the same as dnorm(): return Inf or 0
+	    x.is.mu <- colSums(t(x) != mean) == 0
+	    logretval <- rep.int(-Inf, nrow(x))
+	    logretval[x.is.mu] <- Inf # and all other f(.) == 0
+	    doSolve <- FALSE
         }
     } else {
         dec <- chol(sigma)
     }
-    tmp <- backsolve(dec, t(x) - mean, transpose = TRUE)
-    rss <- colSums(tmp ^ 2)
-    logretval <- - sum(log(diag(dec))) - 0.5 * length(mean) * log(2 * pi) - 0.5 * rss
-    ### />
 
+    if(doSolve) {
+	tmp <- backsolve(dec, t(x) - mean, transpose = TRUE)
+	rss <- colSums(tmp ^ 2)
+	logretval <- - sum(log(diag(dec))) - 0.5 * p * log(2 * pi) - 0.5 * rss
+    }
     names(logretval) <- rownames(x)
-
-    if(log) return(logretval)
-    exp(logretval)
+    if(log) logretval else exp(logretval)
 }
