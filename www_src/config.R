d665be2924151aba23deb3bad0bf1d7d6a3d232d
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-07-09 08:32:25 +0000

    allow local browsing
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mvtnorm@287 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ www_src/config.R
@@ -0,0 +1,8 @@
+
+pkg <- "mvtnorm"
+dest <- "html"
+publish <- "../www"
+
+download.file("http://user.math.uzh.ch/hothorn/TH.bib", dest = "TH.bib")
+system("cat mvtnorm.bib >> TH.bib")
+bib <- "TH.bib"
