efa9d85e21d79effcc48c9a37d3f789be4844e13
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-11-11 15:55:04 +0000

    1.0-1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mvtnorm@291 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/randomF77.c
+++ pkg/src/randomF77.c
@@ -6,7 +6,10 @@
 */
 
 #include <R.h>
+#include <Rmath.h>
 
 void F77_SUB(rndstart)(void) { GetRNGstate(); }
 void F77_SUB(rndend)(void) { PutRNGstate(); }
 double F77_SUB(unifrnd)(void) { return unif_rand(); }
+/* double F77_SUB(sqrtqchisq)(double *n, double *p) { return(sqrt(qchisq(p[0], n[0], 0, 0))); } */
+double F77_SUB(sqrtqchisqint)(int *n, double *p) { return(sqrt(qchisq(p[0], (double) n[0], 0, 0))); }
