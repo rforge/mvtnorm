f1960715a282185247519538e487084a5fdaf7f8
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-02-07 14:14:01 +0000

    replace MVPHI and MVPHNV by pnorm5; 1.1-0
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mvtnorm@357 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/C_FORTRAN_interface.c
+++ pkg/src/C_FORTRAN_interface.c
@@ -14,3 +14,11 @@ double F77_SUB(sqrtqchisqint)(int *n, double *p) {
 double F77_SUB(phid)(double *x){ return pnorm(*x, 0.0, 1.0, 1, 0); }
 double F77_SUB(studnt)(int *nu, double *x){ return pt(x[0], (double) nu[0], 1, 0); }
 
+double F77_SUB(mvphi)(double const *z){
+  return pnorm5(*z, 0., 1., 1L, 0L);
+}
+
+double F77_SUB(mvphnv)(double const *p){
+  return qnorm5(*p, 0., 1., 1L, 0L);
+}
+
